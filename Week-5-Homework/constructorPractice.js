74ba44a90cec8046eff88684f7e83e1890da2db9
Michael Kelly <martisemk@gmail.com>
2022-03-02 16:25:40 -0600

    Final Push for HW 5, Comments Updated

--- Week-5-Homework/constructorPractice.js
+++ Week-5-Homework/constructorPractice.js
@@ -1,4 +1,8 @@
-class genVideoGame { constructor(rating, name, price){
+
+// create base constructor class for video game object
+class genVideoGame { 
+    //add constructor function to create object keys and pass key values, create description variable 
+    constructor(rating, name, price){
     this.rating = rating;
     this.name = name;
     this.price = price;
@@ -8,6 +12,7 @@ class genVideoGame { constructor(rating, name, price){
 
 }
 
+// extend base class for video game and add system console parameter
 class system extends genVideoGame {
     constructor(rating, name, price, consoleSys){
     super(rating, name, price );
@@ -17,19 +22,24 @@ class system extends genVideoGame {
 return `${this.description} Its available on the ${this.consoleSys} console! `
     }
 }
-
+// extend base class for video game and add region parameter
 class region extends genVideoGame {
     constructor(rating, name, price, ...regArea){
+        //inherit parameter properties from parent class
         super(rating, name, price,);
         this.regArea = regArea.join(", ");
     }
     greeting(){
        return `${this.description} Its available in these regions: ${this.regArea} ! `
     }
-}
+} 
+
+//  instantiate new class instance
 const myGame = new genVideoGame("E", "Mario Party", 45);
 const myConsole = new system("M", "Halo 4", 50, "xbox 360")
 const northAmerica = new region("T", "FortNite", 75, "Canada", "USA", "Mexico" )
+
+//Print & invoke class instance 
 console.log(myGame.description);
 console.log(myConsole.greeting());
 console.log(northAmerica.greeting());
